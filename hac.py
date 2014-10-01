1e5bc6d9201b6b4c3331ca44841ed0f47f8a8fd1
Stanislav Khrapov <khrapovs@gmail.com>
2014-10-01 09:33:44 +0400

    hac function has 6.67 score in pylint

--- hac.py
+++ hac.py
@@ -2,18 +2,18 @@
 # -*- coding: utf-8 -*-
 """HAC covariance matrix estimator.
 
-HAC - Heteroscedasticity and Autocorrelation Consistent 
+HAC - Heteroscedasticity and Autocorrelation Consistent
 """
 from __future__ import division
 from math import cos, sin, pi
 import numpy as np
 
-def hac(u, kernel='SU', band=0):
+def hac(vectors, kernel='SU', band=0):
     """HAC estimator of the long-run variance matrix of u.
-    
+
     Parameters
     ----------
-        u: (T, q) array
+        vectors: (T, q) array
             The set of q vectors for estimation of their covariance matrix.
         kernel: str
             Type of kernel.
@@ -21,55 +21,56 @@ def hac(u, kernel='SU', band=0):
         band: int
             Truncation parameter.
             Ideally should be chosen optimally depending on the sample size!
-        
+
     Returns
     -------
         S: (q, q) array
             Long-run variance matrix of u
-        
+
     """
-    T = u.shape[0]
-    
+    length = vectors.shape[0]
+
     # Demean to improve covariance estimate in small samples
     # T x q
-    u = u - u.mean(0)
+    vectors -= vectors.mean(0)
     # q x q
-    S = np.dot(u.T, u) / T
-    
+    covar = np.dot(vectors.T, vectors) / length
+
     for lag in range(band):
-        
+
         # Some constants
-        a = (lag + 1) / (band + 1)
-        d = (lag + 1) / band
-        m = 6 * pi * d / 5
-        
+        a_coef = (lag + 1) / (band + 1)
+        d_coef = (lag + 1) / band
+        m_coef = 6 * pi * d_coef / 5
+
         # Serially Uncorrelated
         if kernel == 'SU':
-            w = 0
+            weight = 0
         # Newey West (1987)
         elif 'Bartlett':
-            if a <= 1:
-                w = 1 - a
+            if a_coef <= 1:
+                weight = 1 - a_coef
             else:
-                w = 0
+                weight = 0
         # Gallant (1987)
         elif kernel == 'Parzen':
-            if a <= .5:
-                w = 1 - 6 * a**2 * (1 - a)
-            elif a <= 1.:
-                w = 2 * (1 - a)**3
+            if a_coef <= .5:
+                weight = 1 - 6 * d_coef**2 * (1 - a_coef)
+            elif a_coef <= 1.:
+                weight = 2 * (1 - a_coef)**3
             else:
-                w = 0
+                weight = 0
         # Andrews (1991)
         elif kernel == 'Quadratic':
-            w = 25 / (12*(d*pi)**2) * (sin(m)/m - cos(m))
-        
+            weight = 25 / (12*(d_coef*pi)**2) \
+                * (sin(m_coef)/m_coef - cos(m_coef))
+
         else:
             raise Exception('Kernel is not yet implemented')
-        
+
         # q x q
-        Gamma = np.dot(u[:-lag-1, :].T, u[lag+1:, :]) / T
+        gamma = np.dot(vectors[:-lag-1, :].T, vectors[lag+1:, :]) / length
         # q x q, w is scalar
-        S += w * (Gamma + Gamma.T)
-    
-    return S
+        covar += weight * (gamma + gamma.T)
+
+    return covar
